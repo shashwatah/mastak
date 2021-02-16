b80a7ab474b0e96fd486893106efaa62dd0978f3
KSSBro <shashwatdeadcliff@gmail.com>
2021-02-16 23:27:40 +0530

    update: src/main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,20 @@
 import fetch from "node-fetch";
 
-export default class Silo {
+interface CacheData {
+    requestData: {
+        url: string,
+        params: any,
+        headers: string,
+        updateInterval: number,
+        dataPro: any
+    },
+    data: any
+};
 
+export default class Silo {
+    private cache: Array<CacheData>;
+
+    constructor() {
+        this.cache = [];
+    }
 }
