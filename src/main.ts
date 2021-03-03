349156b283c946c19f6435175ef74697b5611530
KSSBro <shashwatdeadcliff@gmail.com>
2021-03-03 16:32:40 +0530

    update: src/main.ts

--- src/main.ts
+++ src/main.ts
@@ -8,4 +8,16 @@ export default class Mastak {
     constructor() {
         this.cache = [];
     }
+
+    set(): any {}
+    setMulti(): any {}
+    get(): any {}
+    getMulti(): any {}
+    delete(): any {}
+    deleteMulti(): any {}
+    deleteAll(): any {}
+    update(): any {}
+    has(): any {}
+    returnKeys(): any {}
+    take(): any {}
 }
