942a55e6bd7ae7fe1c396a0d1bc56842974431f7
KSSBro <shashwatdeadcliff@gmail.com>
2021-04-30 23:58:09 +0530

    update: main.ts, main.interfaces.ts: set() rudimentary, interfaces

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import fetch from "node-fetch";
 
-import { Cache } from "./types/main.interfaces";
+import { Cache, CachedAPI } from "./types/main.interfaces";
 
 export default class Mastak {
     private cache: Cache;
@@ -9,7 +9,24 @@ export default class Mastak {
         this.cache = {};
     }
 
-    set(): any {}
+    set(key: string, api: CachedAPI): string {
+        if(!(key in this.cache)) {
+            fetch(api.request.url, {
+                method: api.request.method,
+                body: JSON.stringify(api.request.body),
+                headers: api.request.headers
+            }).then(res => res.json())
+            .then(json => api.dataProcessor(json))
+            .then(processedData => {
+                this.cache.key = api;
+                this.cache.key.data = processedData;  
+            });
+        } else {
+            return("Error");
+        }
+
+        return("Success");
+    }
     setMulti(): any {}
     get(): any {}
     getMulti(): any {}
--- src/types/main.interfaces.ts
+++ src/types/main.interfaces.ts
@@ -1,27 +1,22 @@
-enum requestMethod {
-    GET = 1,
-    POST,
-    PUT,
-    DELETE
-};
-
-export interface Cache {
-    [key: string]: CachedAPI
+export interface Request {
+    url: string,
+    method: string,
+    body?: {
+        [key: string]: string
+    },
+    headers?: {
+        [key: string]: string
+    }
 }
 
 export interface CachedAPI {
-    request: {
-        url: string,
-        method: string,
-        body?: {
-            [key: string]: string
-        },
-        headers?: {
-            [key: string]: string
-        },
-    },
+    request: Request,
     autoUpdate: boolean,
     updateInterval?: number,
     dataProcessor?: any
     data?: any
 };
+
+export interface Cache {
+    [key: string]: CachedAPI
+}
