9d27be10e06e6198ad870bdd04a2e81c2a26bb19
KSSBro <shashwatdeadcliff@gmail.com>
2021-05-01 23:06:46 +0530

    update: src/main.ts: set()

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import fetch from "node-fetch";
 
-import { Cache, CachedAPI } from "./types/main.interfaces";
+import { Cache, CachedAPI, Request } from "./types/main.interfaces";
 
 export default class Mastak {
     private cache: Cache;
@@ -9,24 +9,34 @@ export default class Mastak {
         this.cache = {};
     }
 
-    async set(key: string, api: CachedAPI): Promise<string> {
-        if(!(key in this.cache)) {
-            await fetch(api.request.url, {
-                method: api.request.method,
-                body: JSON.stringify(api.request.body),
-                headers: api.request.headers
-            }).then(res => res.json())
-            .then(json => api.dataProcessor(json))
-            .then(processedData => {
-                this.cache.key = api;
-                this.cache.key.data = processedData;  
-            });
-        } else {
-            return("Error");
-        }
-
-        return("Success");
+    set(key: string, api: CachedAPI): Promise<string> {
+        return new Promise(async (resolve, reject) => {
+            if(!(key in this.cache)) {
+                await fetch(api.request.url, {
+                    method: api.request.method,
+                    ...(("body" in api.request) && {body: JSON.stringify(api.request.body)}),
+                    ...(("headers" in api.request) && {headers: api.request.headers})
+                }).then(res => res.json())
+                .then(json => {
+                    if(api.dataProcessor) {
+                        return(api.dataProcessor(json));
+                    } else {
+                        return(json);
+                    }
+                })
+                .then(processedData => {
+                    this.cache[key] = api;
+                    this.cache[key].data = processedData;  
+                });
+            } else {
+                reject("Error");
+            }
+    
+            resolve("Success");
+            console.log(this.cache);
+        })
     }
+
     setMulti(): any {}
     get(): any {}
     getMulti(): any {}
