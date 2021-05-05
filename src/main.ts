de7745d6bf42812c68a40c06e12aac401e549eae
KSSBro <shashwatdeadcliff@gmail.com>
2021-05-06 04:28:02 +0530

    update: src/main.ts: set(): rework, error handling

--- src/main.ts
+++ src/main.ts
@@ -1,35 +1,57 @@
 import fetch from "node-fetch";
 
-import { Cache, CachedAPI } from "./types/main.interfaces";
+import { Cache, CachedAPI, Errors } from "./types/main.interfaces";
 
 export default class Mastak {
     private cache: Cache;
+    // private errors: Errors;
 
     constructor() {
         this.cache = {};
+        // this.errors = {
+        //     "BadRequest": "The request you input returned an error: _err_",
+        //     "BadProcessor": "The response processor is not working correctly, error: _err_"
+        // };
     }
 
     set(key: string, api: CachedAPI): Promise<string | CachedAPI> {
         return new Promise(async (resolve, reject) => {
             if(!(key in this.cache)) {
-                await fetch(api.request.url, {
-                    method: api.request.method,
-                    ...(("body" in api.request) && {body: JSON.stringify(api.request.body)}),
-                    ...(("headers" in api.request) && {headers: api.request.headers})
-                }).then(res => res.json())
-                .then(json => {
-                    if(api.dataProcessor) {
-                        return(api.dataProcessor(json));
-                    } else {
-                        return(json);
-                    }
-                })
-                .then(processedData => {
-                    this.cache[key] = api;
-                    this.cache[key].data = processedData;  
-                });
+                try {
+                    await fetch(api.request.url, {
+                        method: api.request.method,
+                        ...(("body" in api.request) && {body: JSON.stringify(api.request.body)}),
+                        ...(("headers" in api.request) && {headers: api.request.headers})
+                    }).then(response => response.json())
+                    .then(resJSON => {
+                        console.log(resJSON);
+                        if(api.resProcessor) {
+                            let processedData;
+                            try {
+                                processedData = api.resProcessor(resJSON);
+                            } catch (error) {
+                                let err = new Error();
+                                err.name = "BadProcessor";
+                                err.message = `There's something wrong with the response processor, err: ${error}`;
+                                throw err;
+                            }
+                            
+                            return(processedData);
+                        } else {
+                            return resJSON;
+                        }
+                    })
+                    .then(finalData => {
+                        this.cache[key] = api;
+                        this.cache[key].value = finalData;  
+                    }).catch(err => {
+                        reject(err.message);
+                    })
+                } catch(err) {
+                    reject(`There's something wrong with the request you entered, error: ${err}`);
+                }
             } else {
-                reject("Error");
+                reject("Error: Key already exists");
             }
 
             resolve(this.cache[key]);
@@ -59,5 +81,6 @@ export default class Mastak {
     updateData(): any {}
     checkValues(): any {}
     expire(): any {}
-    error(): any {}
+    generateError(): any {}
+    
 }
