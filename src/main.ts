0141e7da90f26e368f6b7f750a4609dbf73fe298
KSSBro <shashwatdeadcliff@gmail.com>
2021-05-07 04:25:21 +0530

    update: src/main.ts: error handling: generateError() & Errors

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,17 @@
-import fetch from "node-fetch";
+import fetch, { Response }from "node-fetch";
 
 import { Cache, CachedAPI, Errors } from "./types/main.interfaces";
 
 export default class Mastak {
     private cache: Cache;
-    // private errors: Errors;
+    private errors: Errors;
 
     constructor() {
         this.cache = {};
-        // this.errors = {
-        //     "BadRequest": "The request you input returned an error: _err_",
-        //     "BadProcessor": "The response processor is not working correctly, error: _err_"
-        // };
+        this.errors = {
+            "BadRequest": "There's something wrong with the request you entered, error: _err_",
+            "BadProcessor": "There's something wrong with the response processor, error: _err_"
+        };
     }
 
     set(key: string, api: CachedAPI): Promise<string | CachedAPI> {
@@ -30,12 +30,9 @@ export default class Mastak {
                         try {
                             processedData = api.resProcessor(resJSON);
                         } catch (error) {
-                            let err = new Error();
-                            err.name = "BadProcessor";
-                            err.message = `There's something wrong with the response processor, err: ${error}`;
+                            let err = this.generateError("BadProcessor", error);
                             throw err;
                         }
-                        
                         return(processedData);
                     } else {
                         return resJSON;
@@ -65,15 +62,19 @@ export default class Mastak {
         });
     }
 
-    checkResponseStatus(res: any): any {
+    checkResponseStatus(res: Response): Response {
         if(res.ok) {
             return res;
         } else {
-            let error = new Error();
-            error.name = "BadRequest";
-            error.message = `There's something wrong with the request you entered, error: ${res.statusText}`;
-            throw error;
+            throw this.generateError("BadRequest", res.statusText);
         }
+    }
+
+    generateError(type: string, errorMessage: string): Error {
+        let error: Error = new Error();
+        error.name = type;
+        error.message = this.errors[type].replace("_err_", errorMessage);
+        return(error);
     }
 
     // setMulti(): any {}
