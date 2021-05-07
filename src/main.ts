788ef30f917061397011eb4241072f9abcf528ab
KSSBro <shashwatdeadcliff@gmail.com>
2021-05-07 21:27:25 +0530

    update: src/main.ts: error templates

--- src/main.ts
+++ src/main.ts
@@ -10,12 +10,12 @@ export default class Mastak {
     this.cache = {};
     this.errors = {
       BadRequest:
-        "There's something wrong with the request you entered, error: _err_",
+        "There's something wrong with the request; Error Message: _err_",
       BadProcessor:
-        "There's something wrong with the response processor, error: _err_",
+        "There's something wrong with the response processor; Error Message: _err_"
     };
   }
-  
+
   set(key: string, api: CachedAPI): Promise<string | CachedAPI> {
     return new Promise(async (resolve, reject) => {
       if (!(key in this.cache)) {
@@ -26,7 +26,7 @@ export default class Mastak {
           }),
           ...("headers" in api.request && { headers: api.request.headers }),
         })
-          .then((response) => this.checkResponseStatus(response))
+          .then((response) => this._checkResponseStatus(response))
           .then((response) => response.json())
           .then((resJSON) => {
             console.log(resJSON);
@@ -35,7 +35,7 @@ export default class Mastak {
               try {
                 processedData = api.resProcessor(resJSON);
               } catch (error) {
-                let err = this.generateError("BadProcessor", error);
+                let err = this._generateError("BadProcessor", error);
                 throw err;
               }
               return processedData;
@@ -51,7 +51,7 @@ export default class Mastak {
             reject(err.message);
           });
       } else {
-        reject("Error: Key already exists");
+        reject("Error: The Key entered already exists");
       }
 
       resolve(this.cache[key]);
@@ -63,23 +63,23 @@ export default class Mastak {
       if (key in this.cache) {
         resolve(this.cache[key]);
       } else {
-        reject("Data not found");
+        reject("Error: Data does not exist");
       }
     });
   }
 
-  checkResponseStatus(res: Response): Response {
+  _checkResponseStatus(res: Response): Response {
     if (res.ok) {
       return res;
     } else {
-      throw this.generateError("BadRequest", res.statusText);
+      throw this._generateError("BadRequest", res.statusText);
     }
   }
 
-  generateError(type: string, errorMessage: string): Error {
+  _generateError(type: string, errorMessage: string): Error {
     let error: Error = new Error();
     error.name = type;
-    error.message = this.errors[type].replace("_err_", errorMessage);
+    error.message = `Error: ${this.errors[type].replace("_err_", errorMessage)}`;
     return error;
   }
 
